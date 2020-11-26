53cf0891a65d410b2cb343995e833b12219998b6
flaskoski <felipeflaskoski@outlook.com>
2020-11-26 00:24:02 -0300

    runApp.sh stops the application running on background and starts the new version

--- Scripts/runApp.sh
+++ Scripts/runApp.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /tmp
-java -jar -Dspring.profiles.active=prod Transactions-0.1-SNAPSHOT.jar &
+FILE=./app.pid
+if [ -f "$FILE" ]; then
+  kill $(<$FILE)
+fi
+nohup java -jar -Dspring.profiles.active=prod Transactions-0.1-SNAPSHOT.jar 2>&1 >> logfile.log &
+echo $! > $FILE
