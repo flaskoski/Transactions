76b4f285ebd048b6d809dc12e4a960b11e01e1df
flaskoski <felipeflaskoski@outlook.com>
2020-11-28 20:24:33 -0300

    runApp.sh runs in background

--- Scripts/runApp.sh
+++ Scripts/runApp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp
 FILE=./app.pid
-java -jar -Dspring.profiles.active=prod Transactions-0.1-SNAPSHOT.jar 2>&1 >> logfile.log
+java -jar -Dspring.profiles.active=prod Transactions-0.1-SNAPSHOT.jar 2>&1 >> logfile.log &
 echo $! > $FILE
